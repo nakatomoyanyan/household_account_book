24e59c55f4b7b5dd936e271399d2faab9af88d7d
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-11 15:21:33 +0900

    マイグレーションファイルの編集
    ユーザー名を登録できるようにしました。

--- household_account_book_app/db/schema.rb
+++ household_account_book_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_11_060036) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_11_061638) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
