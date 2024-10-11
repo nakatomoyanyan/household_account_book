64808bbcec7ec3a3dc4127eded3b1768becce88c
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-11 15:49:02 +0900

    新規ユーザー作成メソッドの実装
    1.user_paramsメソッドでユーザー登録画面からUserモデルへ送信されたパラメーターを要求し、その中からpermitで指定したパラメータを取得
    2.user_paramsメソッドで取得した情報を元にUserクラスのインスタンスを生成
    3.インスタンスの保存に成功したらログインページに移動
    4.失敗したらnewメソッドに対応するview(デフォルトのnew.html.erb)に移動
    以上

--- household_account_book_app/app/controllers/users_controller.rb
+++ household_account_book_app/app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      render 'new'
+    end
+  end
+  private
+    def user_params
+       params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+    end
 end
