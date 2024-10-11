21f3a16978234984a68fd5f759ce77c5713ec967
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-11 15:29:55 +0900

    Userモデルにバリデーションを追加
    以下のバリデーションを実装しました。
    ・ユーザーが設定したパスワードの長さが5文字以上であることを確認
    ・ユーザーが設定したパスワードと確認用のパスワードが一致していることを確認
    ・確認用パスワードの入力が存在することを確認
    ・新規にユーザーが登録された際に、emailが存在するか、登録済みかどうかを確認
    ・新規にユーザーが登録された際に、user_nameが存在するかどうかの確認と文字数の制限
    以上

--- household_account_book_app/app/models/user.rb
+++ household_account_book_app/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :password, length: { minimum: 5 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :user_name, presence: true, length: { maximum: 20 }
 end
