e65b4c6b5033dfcac4d5f0685705fb9a17eeb12a
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-18 12:50:22 +0900

    バリデーションのリファクタリング
    同じ属性へのvalidatesをまとめて書きました。

--- household_account_book_app/app/models/user.rb
+++ household_account_book_app/app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  validates :password, length: { minimum: 5 }
-  validates :password, confirmation: true
+  validates :password, length: { minimum: 5 }, confirmation: true
   validates :password_confirmation, presence: true
   validates :email, uniqueness: true, presence: true
   validates :user_name, presence: true, length: { maximum: 20 }
