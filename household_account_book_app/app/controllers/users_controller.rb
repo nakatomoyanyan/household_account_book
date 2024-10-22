2d199ab99eff4c21b82fd01ecb1bbf07d3ec1256
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-22 15:01:34 +0900

    SignUp後に自動でログインするように変更
    SignUp後に自動でログインするように変更しました。

--- household_account_book_app/app/controllers/users_controller.rb
+++ household_account_book_app/app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      auto_login(@user)
       flash[:success] = "家計簿アプリへようこそ！"
-      redirect_to static_pages_home_url #仮のURL
+      redirect_to '/static_pages/home'
     else
       render 'new', status: :unprocessable_entity
     end
