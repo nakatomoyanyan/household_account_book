ddaa4ef99719520473cc4c77138d56a2baab3908
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-22 16:17:54 +0900

    factorybotの設定の変更
    ユーザー名とemailをfakerを使わずに指定しました。理由はSignInのテストのためです。

--- household_account_book_app/spec/factories/users.rb
+++ household_account_book_app/spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    user_name                        {Faker::Internet.username}
-    email                           {Faker::Internet.email}
+    user_name                       {'testuser'}
+    email                           {'test@example.com'}
     password                        {'abc1234'}
     password_confirmation           {'abc1234'}
   end
