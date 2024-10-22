0e143c56401863547454f0519b3be74884f20d9b
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-22 14:45:20 +0900

    SignIn機構の実装
    sessions_controllerにcreateアクションとdestroyアクションを書きました。サインイン成功時、失敗時にflashを表示するようにしました。

---
+++ household_account_book_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      flash[:notice] = 'サインインに成功しました'
+      redirect_to '/static_pages/home'
+    else
+      flash[:notice] = 'サインインに失敗しました'
+      redirect_to '/static_pages/home'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to '/static_pages/home'
+  end
+end
