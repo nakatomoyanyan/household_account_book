362ba89e301c7c2b98bd7529d840a5371c2e1496
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-22 11:27:35 +0900

    sessions_controllerへのルーティングの設定
    sessions_controllerのアクションへのルーティングを設定しました。

--- household_account_book_app/config/routes.rb
+++ household_account_book_app/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,5 +15,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "static_pages/home", to: "static_pages#home"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   resources :users
 end
