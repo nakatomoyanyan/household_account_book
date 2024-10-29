06f23f13b939ebcbf289cfb1d5a7f1f34ce93264
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-29 15:20:43 +0900

    categories_controllerのリファクタリング
    set_xxx 系の処理をbefore_actionではなくメモ化して関数参照にしました

--- household_account_book_app/app/controllers/categories_controller.rb
+++ household_account_book_app/app/controllers/categories_controller.rb
@@ -1,38 +1,36 @@
 class CategoriesController < ApplicationController
-  before_action :set_user
   before_action :authorize_user
-  before_action :set_category, only: [:destroy]
 
   def new
-    @category = @user.categories.new
-    @categories = @user.categories.all
+    @category = user.categories.new
+    @categories = user.categories.all
   end
 
   def create
-    @category = @user.categories.new(category_params)
+    @category = user.categories.new(category_params)
 
     flash[:notice] = if @category.save
                        '登録に成功しました'
                      else
                        '登録に失敗しました'
                      end
-    redirect_to user_categories_path(@user)
+    redirect_to user_categories_path(user)
   end
 
   def destroy
-    @category.destroy
+    category.destroy
     flash[:notice] = '名目が削除されました'
-    redirect_to user_categories_path(@user)
+    redirect_to user_categories_path(user)
   end
 
   private
 
-  def set_user
-    @user = User.find(params[:user_id])
+  def user
+    @user ||= User.find(params[:user_id])
   end
 
-  def set_category
-    @category = @user.categories.find_by(id: params[:id])
+  def category
+    @category ||= User.categories.find_by(id: params[:id])
   end
 
   def category_params
@@ -40,7 +38,7 @@ class CategoriesController < ApplicationController
   end
 
   def authorize_user
-    return if @user == current_user
+    return if user == current_user
 
     flash[:notice] = 'アクセス権限がありません'
     redirect_to root_path
