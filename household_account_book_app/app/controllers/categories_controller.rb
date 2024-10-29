f0048ec60a242b03673d6abb46015754527c36de
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-29 11:48:35 +0900

    categories_controllerのアクション、viewの実装
    #create,#new,#destroyを実装しました。合わせて仮に実装していた画面のレイアウトを変更しました。

--- household_account_book_app/app/controllers/categories_controller.rb
+++ household_account_book_app/app/controllers/categories_controller.rb
@@ -1,20 +1,29 @@
 class CategoriesController < ApplicationController
   before_action :set_user
+  before_action :authorize_user
+  before_action :set_category, only: [:destroy]
 
   def new
     @category = @user.categories.new
+    @categories = @user.categories.all
   end
 
   def create
     @category = @user.categories.new(category_params)
 
     if @category.save
-      flash[:notice] = "登録に成功しました" 
+      flash[:notice] = '登録に成功しました'
       redirect_to user_categories_path(@user)
     else
-      flash[:notice] = "登録に失敗しました"
+      flash[:notice] = '登録に失敗しました'
       redirect_to user_categories_path(@user)
     end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:notice] = '名目が削除されました'
+    redirect_to user_categories_path(@user)
   end
 
   private
@@ -22,7 +31,18 @@ class CategoriesController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
+  def set_category
+    @category = @user.categories.find_by(id: params[:id])
+  end
+
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def authorize_user
+    unless @user == current_user
+      flash[:notice] = 'アクセス権限がありません' 
+      redirect_to root_path
+    end
   end
 end
