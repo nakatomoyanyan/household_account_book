a3a5570bdaf2058f7dda94cc404e036be10025f0
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-10-31 15:24:24 +0900

    householdモデルにscopeとメソッドを設定
    DBから今年と今月の収支に関連する情報を取得するために、householdモデルにscopeとメソッドを設定しました。

--- household_account_book_app/app/models/household.rb
+++ household_account_book_app/app/models/household.rb
@@ -7,4 +7,33 @@ class Household < ApplicationRecord
   validates :date, presence: true
   validates :transaction_type, presence: true
   validates :amount, presence: true
+
+  scope :this_year, -> { where(date: Date.current.beginning_of_year..Date.current.end_of_year) }
+  scope :this_month, -> { where(date: Date.current.beginning_of_month..Date.current.end_of_month) } 
+  scope :income, -> { where(transaction_type: 0) }
+  scope :expense, -> { where(transaction_type: [1, 2]) }
+
+  def self.total_income_this_year
+    this_year.income.sum(:amount)
+  end
+
+  def self.total_expense_this_year
+    this_year.expense.sum(:amount)
+  end
+
+  def self.net_balance_this_year
+    total_income_this_year - total_expense_this_year
+  end
+
+  def self.total_income_this_month
+    this_month.income.sum(:amount)
+  end
+
+  def self.total_expense_this_month
+    this_month.expense.sum(:amount)
+  end
+
+  def self.net_balance_this_month
+    total_income_this_month - total_expense_this_month
+  end
 end
