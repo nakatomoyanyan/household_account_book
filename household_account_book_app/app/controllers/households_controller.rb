4cbaaadce72cc3f867c9bb7429bc4d8fa5da2b7b
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-11-11 12:22:36 +0900

    重複している Relation を一時変数に入れる
    コードを見やすくするために重複している Relation を一時変数に入れました。

--- household_account_book_app/app/controllers/households_controller.rb
+++ household_account_book_app/app/controllers/households_controller.rb
@@ -18,10 +18,10 @@ class HouseholdsController < ApplicationController
   end
 
   def income
-    @incomes_this_year = current_user.households.income.this_year
-    @incomes_grath_data_this_year = current_user.households.income.this_year.group_by_month(:date,
-                                                                                            format: '%B').sum(:amount)
-    @incomes_grath_data_this_month = current_user.households.income.this_month.group(:name).sum(:amount)
+    income = current_user.households.income
+    @incomes_this_year = income.this_year
+    @incomes_grath_data_this_year = @incomes_this_year.group_by_month(:date, format: '%B').sum(:amount)
+    @incomes_grath_data_this_month = income.this_month.group(:name).sum(:amount)
   end
 
   private
