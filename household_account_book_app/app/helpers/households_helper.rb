12754a601414363a6bf9bc6152893eeed78e1169
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-11-12 18:25:33 +0900

    支出一覧の分類を日本語化するhelper
    viewはあくまで画面にどのように表示するかだけ関心があると考え、hlperに書きました。

--- household_account_book_app/app/helpers/households_helper.rb
+++ household_account_book_app/app/helpers/households_helper.rb
@@ -2,4 +2,8 @@ module HouseholdsHelper
   def transaction_type_options
     Household.transaction_types.keys.map { |key| [I18n.t("transaction_type.#{key}"), key] }
   end
+
+  def transaction_type_label(transaction_type)
+    I18n.t("transaction_type.#{transaction_type}")
+  end
 end
