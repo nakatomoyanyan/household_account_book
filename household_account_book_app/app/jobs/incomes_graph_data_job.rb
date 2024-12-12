c2650747caf53ed52fda18a49c51f23efed6a8da
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-12-12 14:58:44 +0900

    jobで収入のグラフ用のデータを収集＆データ整形
    jobで収入のグラフ用のデータを収集して、収集したデータをIncomesGrathテーブルに保存

---
+++ household_account_book_app/app/jobs/incomes_graph_data_job.rb
@@ -0,0 +1,17 @@
+class IncomesGraphDataJob < ApplicationJob
+  queue_as :default
+
+  def perform(user_id)
+    Rails.logger.info('IncomesGrathDataJob started')
+    current_user = User.find(user_id)
+    incomes = current_user.households.income
+    incomes_grath_data_this_month = incomes.this_month.joins(:category).group('categories.name').sum(:amount)
+    incomes_grath_data_this_year = incomes.this_year.group_by_month(:date, format: '%B').sum(:amount)
+    IncomesGrath.create!(
+      user: current_user,
+      grath_data_this_month: incomes_grath_data_this_month,
+      grath_data_this_year: incomes_grath_data_this_year
+    )
+    Rails.logger.info('IncomesGrathDataJob finished')
+  end
+end
