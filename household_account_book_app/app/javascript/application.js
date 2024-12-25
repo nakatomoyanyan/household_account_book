7bb6654ad737c846d884d2d7d0e5dee65db46be9
nakatomoyanyan <nakatomorelativeb@yahoo.ne.jp>
2024-12-25 15:09:25 +0900

    sccript読み込みの関数化＆income画面表示時のみにjsを動かすように変更

--- household_account_book_app/app/javascript/application.js
+++ household_account_book_app/app/javascript/application.js
@@ -2,7 +2,7 @@ import "@hotwired/turbo-rails";
 import "controllers";
 import "chartkick";
 import "Chart.bundle";
-
+let shouldRetry = true;
 async function fetchIncomesData() {
   try {
     const response = await fetch("/households/collecting_incomes_grath_data");
@@ -18,30 +18,17 @@ async function fetchIncomesData() {
         ".grath-income-this-year-content"
       );
       AddedChartThisYearContainer.innerHTML = data.html_year;
-      const scriptsThisYear =
-        AddedChartThisYearContainer.querySelectorAll("script");
-      scriptsThisYear.forEach((script) => {
-        const newScript = document.createElement("script");
-        newScript.textContent = script.innerHTML;
-        document.body.appendChild(newScript);
-        document.body.removeChild(newScript);
-      });
 
       const AddedChartThisMonthContainer = document.querySelector(
         ".grath-income-this-month-content"
       );
       AddedChartThisMonthContainer.innerHTML = data.html_month;
-      const scriptsThisMonth =
-        AddedChartThisMonthContainer.querySelectorAll("script");
-      scriptsThisMonth.forEach((script) => {
-        const newScript = document.createElement("script");
-        newScript.textContent = script.innerHTML;
-        document.body.appendChild(newScript);
-        document.body.removeChild(newScript);
-      });
+
+      executeScriptsFromContainer(AddedChartThisYearContainer);
+      executeScriptsFromContainer(AddedChartThisMonthContainer);
     } else if (data.status === "in_progress") {
       console.log("データ収集中。数秒後に再試行します...");
-      setTimeout(fetchIncomesData, 10000);
+      setTimeout(fetchIncomesData(), 10000);
     } else {
       console.warn("未定義のステータス:", data.status);
     }
@@ -50,4 +37,19 @@ async function fetchIncomesData() {
   }
 }
 
-fetchIncomesData();
+function executeScriptsFromContainer(container) {
+  const scripts = container.querySelectorAll("script");
+  scripts.forEach((script) => {
+    const newScript = document.createElement("script");
+    newScript.textContent = script.innerHTML;
+    document.body.appendChild(newScript);
+    document.body.removeChild(newScript);
+  });
+}
+
+document.addEventListener("turbo:load", function () {
+  const currentUrl = window.location.pathname;
+  if (currentUrl.includes("/households/income")) {
+    fetchIncomesData();
+  }
+});
